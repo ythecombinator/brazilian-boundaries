9e37058baa40851950b100c7fb13f5270d687325
Matheus Brasil <matheus.brasil10@gmail.com>
2017-01-25 07:08:04 -0200

    feat(list): list boundary file paths

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const fs    = require('graceful-fs')
+const join  = require('path').join
+
+const dir   = join(__dirname, 'files')
+
+const fin = (resolve, reject, cb) => (err, res) => {
+  
+  if (err) {
+    reject(err)
+    if (cb) cb(err)
+    return
+  }
+
+  resolve(res)
+  
+  if (cb) cb(null, res)
+}
+
+export const list = (cb) =>
+
+  new Promise((resolve, reject) => {
+
+    const done = fin(resolve, reject, cb)
+
+    fs.readdir(dir, (err, res) => {
+      if (err) return done(err)
+      done(null, res.map((f) => join(dir, f)))
+    })
+
+  })
